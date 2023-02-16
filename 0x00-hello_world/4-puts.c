50af10f3dd30feeec71992886bf3eec9756ca8ac
Nadia Ibrahim <larynadia70@gmail.com>
2023-02-16 08:06:29 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a line with put function
+ * return: 0 (success)
+ */
+int main(void)
+	(
+	 puts("\"Programming is like building a multilingual puzzle");
+return (0);
