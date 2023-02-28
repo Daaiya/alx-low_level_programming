8b77a0aa37bd3f2f8dd68be8ace2a8422d710359
Nadia Ibrahim <larynadia70@gmail.com>
2023-02-28 11:22:11 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,11 +4,11 @@
  * @a: integer to swap
  * @b: integer to swap
  */
-void swap_int(int *s, int *b)
+void swap_int(int *a, int *b)
 {
-int a;
+int s;
 
-a = *s;
-*s = *b;
+s = *a;
+*a = *b;
 *b = s;
 }
